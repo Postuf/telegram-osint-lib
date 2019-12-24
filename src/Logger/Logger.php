237bee14c0346c5532301a6f5a429b0127a88eac
wkejqb <58689118+wkejqb@users.noreply.github.com>
2019-12-24 15:26:43 +0300

    Controllable logging (#4)
    - added ability to suppress send/rcvd messages body output using cli flag --info

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -12,6 +12,13 @@ class Logger
      */
     private static $logger;
 
+    private const DEBUG_LABELS = [
+        'Read_Message_Binary',
+        'Read_Message_TL',
+        'Write_Message_Binary',
+        'Write_Message_TL',
+        'Write_Message_ID',
+    ];
 
     /**
      * @param string $label
@@ -19,8 +26,16 @@ class Logger
      */
     public static function log($label, $log)
     {
-        if(LibConfig::LOGGER_ENABLED)
+        global $argv;
+        if(LibConfig::LOGGER_ENABLED) {
+            $hasDebugFlag = isset($argv[count($argv) - 1]) && $argv[count($argv) - 1] == '--debug';
+            $hasInfoFlag = isset($argv[count($argv) - 1]) && $argv[count($argv) - 1] == '--info';
+            $isDebug = (LibConfig::LOG_LEVEL === 'debug' || $hasDebugFlag) && !$hasInfoFlag;
+            if (!$isDebug && in_array($label, self::DEBUG_LABELS, true)) {
+                return;
+            }
             self::getLogger()->debugLibLog($label, $log);
+        }
     }
 
 
