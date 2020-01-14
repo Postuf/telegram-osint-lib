082beb0b41eda14b7dcb557fa4b95fe3c4af42af
wkejqb <58689118+wkejqb@users.noreply.github.com>
2020-01-14 16:25:10 +0300

    "Parse channel photos" scenario (#22)
    * 'Parse photos from channel' scenario
    
     - get channel by username;
     - include messages from date range if supplied;
     - return photo:timestamp objects.
     - add group members scenario test

--- src/Scenario/GroupMembersClient.php
+++ src/Scenario/GroupMembersClient.php
@@ -9,6 +9,7 @@ use Closure;
 use Exception\TGException;
 use Logger\Logger;
 use MTSerialization\AnonymousMessage;
+use SocksProxyAsync\Proxy;
 use TLMessage\TLMessage\ClientMessages\Api\get_all_chats;
 use TLMessage\TLMessage\ClientMessages\Api\get_full_chat;
 
@@ -18,28 +19,35 @@ use TLMessage\TLMessage\ClientMessages\Api\get_full_chat;
  * This example requires info user (second authkey)
  * to be member of one or several groups, otherwise it is useless.
  *
+ * Please note that for public groups you need to be group admin to see member list.
+ *
  * @see get_all_chats
  * @see get_full_chat
  */
-class GroupMembersClient extends MyTgClientDebug implements ScenarioInterface
+class GroupMembersClient extends AbstractGroupClient implements ScenarioInterface
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
+    /** @var callable|null */
+    private $handler;
 
     /**
+     * @param callable                      $handler   function()
+     * @param Proxy|null                    $proxy
+     * @param ClientGeneratorInterface|null $generator
+     *
      * @throws TGException
      */
-    public function startActions(): void
+    public function __construct(
+        callable $handler = null,
+        ?Proxy $proxy = null,
+        ?ClientGeneratorInterface $generator = null
+    ) {
+        parent::__construct($proxy, $generator);
+        $this->handler = $handler;
+    }
+
+    private function getAllChatsHandler(): callable
     {
-        $this->infoLogin();
-        Logger::log(__CLASS__, 'sleep(2)');
-        sleep(2);
-        Logger::log(__CLASS__, 'getting all chats');
-        /** @var array $ids */
-        $ids = [];
-        $this->infoClient->getAllChats(function (AnonymousMessage $message) use (&$ids) {
+        return function (AnonymousMessage $message) {
             /** @see https://core.telegram.org/constructor/messages.chats */
             $chats = $message->getNodes('chats');
             $chatCount = count($chats);
@@ -50,12 +58,37 @@ class GroupMembersClient extends MyTgClientDebug implements ScenarioInterface
                     Logger::log(__CLASS__, 'Skipped node of type '.$chatNode->getType());
                     continue;
                 }
-                $ids[] = $id;
 
                 sleep(1);
                 $this->infoClient->getChatMembers($id, $this->makeChatMemberHandler($id));
             }
-        });
+        };
+    }
+
+    /**
+     * @throws TGException
+     */
+    public function startActions(): void
+    {
+        $this->infoLogin();
+        sleep(1);
+        Logger::log(__CLASS__, 'getting all chats');
+        if ($this->deepLink) {
+            Logger::log(__CLASS__, "getting chat by deeplink {$this->deepLink}");
+            $parts = explode('/', $this->deepLink);
+            $username = $parts[count($parts) - 1];
+            $this->infoClient->resolveUsername($username, $this->getResolveHandler(function (AnonymousMessage $message) {
+                $chats = $message->getValue('chats');
+                foreach ($chats as $chat) {
+                    $id = (int) $chat['id'];
+                    Logger::log(__CLASS__, "getting channel members for channel $id");
+                    /** @var array $chat */
+                    $this->infoClient->getChannelMembers($id, $chat['access_hash'], $this->makeChatMemberHandler($id));
+                }
+            }));
+        } else {
+            $this->infoClient->getAllChats($this->getAllChatsHandler());
+        }
 
         $this->pollAndTerminate();
     }
@@ -69,19 +102,32 @@ class GroupMembersClient extends MyTgClientDebug implements ScenarioInterface
         return function (AnonymousMessage $message) use ($id) {
             /** @see https://core.telegram.org/constructor/messages.chatFull */
             $users = $message->getNodes('users');
-            foreach ($users as $user) {
-                $userId = (int) $user->getValue('id');
-                $phone = (string) $user->getValue('phone');
-                $phoneSuffix = $phone ? " with phone $phone" : '';
-                Logger::log(__CLASS__, "chat $id contains user $userId$phoneSuffix");
+            if ($this->handler) {
+                $models = [];
+                foreach ($users as $user) {
+                    $model = new UserInfoModel();
+                    $model->id = (int) $user->getValue('id');
+                    $model->phone = (string) $user->getValue('phone');
+                    $model->username = (string) $user->getValue('username');
+                    $models[] = $model;
+                }
+                $handler = $this->handler;
+                $handler($models);
+            } else {
+                foreach ($users as $user) {
+                    $userId = (int) $user->getValue('id');
+                    $phone = (string) $user->getValue('phone');
+                    $phoneSuffix = $phone ? " with phone $phone" : '';
+                    Logger::log(__CLASS__, "chat $id contains user $userId$phoneSuffix");
 
-                if ($phone) {
-                    $this->infoClient->getInfoByPhone(
-                        $phone,
-                        false,
-                        false,
-                        Closure::fromCallable([$this, 'userInfoHandler'])
-                    );
+                    if ($phone) {
+                        $this->infoClient->getInfoByPhone(
+                            $phone,
+                            false,
+                            false,
+                            Closure::fromCallable([$this, 'userInfoHandler'])
+                        );
+                    }
                 }
             }
         };
--- src/Scenario/GroupPhotosClient.php
+++ src/Scenario/GroupPhotosClient.php
@@ -23,16 +23,12 @@ use TLMessage\TLMessage\ClientMessages\Api\get_history;
  * @see get_all_chats
  * @see get_history
  */
-class GroupPhotosClient extends MyTgClientDebug implements ScenarioInterface
+class GroupPhotosClient extends AbstractGroupClient implements ScenarioInterface
 {
-    /** @var int|null */
-    private $groupId;
     /** @var int|null */
     private $since;
     /** @var int|null */
     private $to;
-    /** @var string|null */
-    private $deepLink;
     /** @var callable */
     private $saveHandler;
 
@@ -54,16 +50,6 @@ class GroupPhotosClient extends MyTgClientDebug implements ScenarioInterface
         $this->since = $since;
         $this->to = $to;
         $this->saveHandler = $saveHandler;
-    }
-
-    public function setGroupId(int $groupId): void
-    {
-        $this->groupId = $groupId;
-    }
-
-    public function setDeepLink(string $deepLink): void
-    {
-        $this->deepLink = $deepLink;
     }
 
     private function getSinceTs(): int
@@ -118,6 +104,49 @@ class GroupPhotosClient extends MyTgClientDebug implements ScenarioInterface
         });
     }
 
+    private function getAllChatsHandler(int $limit): callable
+    {
+        return function (AnonymousMessage $message) use ($limit)
+        {
+            /** @see https://core.telegram.org/constructor/messages.chats */
+            $chats = $message->getNodes('chats');
+            $chatCount = count($chats);
+            Logger::log(__CLASS__, "got $chatCount chats");
+            foreach ($chats as $chatNode) {
+                $id = (int) $chatNode->getValue('id');
+                if ($this->groupId && $this->groupId != $id) {
+                    continue;
+                }
+
+                if ($chatNode->getType() != 'chat' && !$this->groupId) {
+                    continue;
+                }
+
+                $handler = $this->makeChatMessagesHandler($id, $limit);
+                Logger::log(__CLASS__, "parsing {$chatNode->getType()} $id with limit $limit");
+                if ($chatNode->getType() == 'chat') {
+                    $this->infoClient->getChatMessages(
+                        $id,
+                        $limit,
+                        0,
+                        0,
+                        $handler
+                    );
+                } else {
+                    $accessHash = $chatNode->getValue('access_hash');
+                    $this->infoClient->getChannelMessages(
+                        $id,
+                        $accessHash,
+                        $limit,
+                        0,
+                        0,
+                        $handler
+                    );
+                }
+            }
+        };
+    }
+
     /**
      * @throws TGException
      */
@@ -125,27 +154,13 @@ class GroupPhotosClient extends MyTgClientDebug implements ScenarioInterface
     {
         $this->infoLogin();
         /** @var array $ids */
-        $ids = [];
         $limit = 200;
         sleep(1);
         if ($this->deepLink) {
             Logger::log(__CLASS__, "getting chat by deeplink {$this->deepLink}");
             $parts = explode('/', $this->deepLink);
             $username = $parts[count($parts) - 1];
-            $this->infoClient->resolveUsername($username, function (AnonymousMessage $message) use ($limit) {
-                if ($message->getType() !== 'contacts.resolvedPeer') {
-                    Logger::log(__CLASS__, 'got unexpected response of type '.$message->getType());
-
-                    return;
-                }
-                /** @var array $peer */
-                $peer = $message->getValue('peer');
-                if ($peer['_'] !== 'peerChannel') {
-                    Logger::log(__CLASS__, 'got unexpected peer of type '.$peer['_']);
-
-                    return;
-                }
-
+            $this->infoClient->resolveUsername($username, $this->getResolveHandler(function (AnonymousMessage $message) use ($limit) {
                 $chats = $message->getValue('chats');
                 foreach ($chats as $chat) {
                     $id = (int) $chat['id'];
@@ -160,51 +175,11 @@ class GroupPhotosClient extends MyTgClientDebug implements ScenarioInterface
                         0,
                         $handler
                     );
-
                 }
-            });
+            }));
         } else {
             Logger::log(__CLASS__, 'getting all chats');
-            $this->infoClient->getAllChats(function (AnonymousMessage $message) use (&$ids, $limit) {
-                /** @see https://core.telegram.org/constructor/messages.chats */
-                $chats = $message->getNodes('chats');
-                $chatCount = count($chats);
-                Logger::log(__CLASS__, "got $chatCount chats");
-                foreach ($chats as $chatNode) {
-                    $id = (int) $chatNode->getValue('id');
-                    $ids[] = $id;
-                    if ($this->groupId && $this->groupId != $id) {
-                        continue;
-                    }
-
-                        if ($chatNode->getType() != 'chat' && !$this->groupId) {
-                            continue;
-                        }
-
-                        $handler = $this->makeChatMessagesHandler($id, $limit);
-                        Logger::log(__CLASS__, "parsing {$chatNode->getType()} $id with limit $limit");
-                        if ($chatNode->getType() == 'chat') {
-                            $this->infoClient->getChatMessages(
-                                $id,
-                                $limit,
-                                0,
-                                0,
-                                $handler
-                            );
-                        } else {
-                            $accessHash = $chatNode->getValue('access_hash');
-                            $this->infoClient->getChannelMessages(
-                                $id,
-                                $accessHash,
-                                $limit,
-                                0,
-                                0,
-                                $handler
-                            );
-                        }
-                    }
-                }
-            );
+            $this->infoClient->getAllChats($this->getAllChatsHandler($limit));
         }
 
         $this->pollAndTerminate();
@@ -279,7 +254,12 @@ class GroupPhotosClient extends MyTgClientDebug implements ScenarioInterface
                 $saveHandler = $this->saveHandler ?: function (PictureModel $pictureModel, int $id) {
                     $filename = "$id.".$pictureModel->format;
                     file_put_contents($filename, $pictureModel->bytes);
-                    Logger::log(__CLASS__, "$filename saved with time ".date('Y-m-d H:i:s', $pictureModel->modificationTime).' '.date_default_timezone_get());
+                    Logger::log(
+                        __CLASS__,
+                        "$filename saved with time ".
+                        date('Y-m-d H:i:s', $pictureModel->modificationTime).
+                        ' '.date_default_timezone_get()
+                    );
                 };
                 $fileModel = new FileModel(
                     (int) $photo['id'],
