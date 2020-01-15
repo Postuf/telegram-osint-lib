ecfdf54a33a9fbefe25a592a6364cc42f997b26e
wkejqb <58689118+wkejqb@users.noreply.github.com>
2020-01-15 13:40:29 +0300

    Fix composer autoload (#27)

--- src/Exception/ConstantCodesGeneration.php
+++ src/Exception/ConstantCodesGeneration.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use Exception\TGException;
+require_once __DIR__.'/../../vendor/autoload.php';
 
-require_once __DIR__ . "/../../ClassLoader.php";
+use Exception\TGException;
 
 $codeGroup = 100000;
 $localCounter = 0;
@@ -22,5 +22,4 @@ foreach((new TGException())->getConstants() as $code => $constant){
     $name = 'const '.$constant;
     $len = strlen($name);
     echo $name . str_pad('', 75-$len, " ") . '= '.$calculatedCode.";\n";
-
 }
