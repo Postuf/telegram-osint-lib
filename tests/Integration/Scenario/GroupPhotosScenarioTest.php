90d3c74e584d259a2c4c7a2287339a083881d111
Stanislav V <vorobiev.s@postuf.com>
2020-01-27 16:44:37 +0300

    Filter photos by username scenario

--- tests/Integration/Scenario/GroupPhotosScenarioTest.php
+++ tests/Integration/Scenario/GroupPhotosScenarioTest.php
@@ -17,6 +17,7 @@ class GroupPhotosScenarioTest extends TestCase
     private const DEFAULT_AUTHKEY = '79803100357:7b22646576696365223a2273616d73756e6747542d4e38303030222c22616e64726f696453646b56657273696f6e223a2253444b203231222c2266697273744e616d65223a226e616d653538666630303735222c226c6173744e616d65223a226c6173746e616d656264623634623966222c226465766963654c616e67223a2272752d7275222c226170704c616e67223a227275222c2261707056657273696f6e223a22342e382e3131222c2261707056657273696f6e436f6465223a223133313831222c226c6179657256657273696f6e223a38327d:XrYgWUps5khLnDLE/5c9buuAMLQsIqjv8WyPriN0bZ1ePREdBPPfdbc0W+Fvr+KKRsg8lm+D8mvoe/tcwQ9SX1hyGqqu0Qc7HtRr9Y+OI1UL47CH/UdMDhaeMMdPIulMGrTLJJJW0bG3IFnLC+5hUkk7gH90agg4WGzNjBgz3e90aZ3nsgovefrQLT2549aklGOW3+rbXBuID3iIOLu+A1hafuwqhS3Z3TGi1AuYqZcGxDzZVOn9OlFjVBv2/c+VeqiwTDqwg5Pq79edMPBxluOrXUOEaZDBXcqznDwk1lJ7zhgX/cHHU9isrgdzO4qzt+gNZ71ybYx1JxRrx6P9/A==:7b2263726561746564223a313533323434373530342c226170695f6964223a362c2264635f6964223a322c2264635f6970223a223134392e3135342e3136372e3530222c2264635f706f7274223a3434337d';
     private const DEFAULT_TRACE_PATH = '/traces/group-photos.txt';
     private const TRACE_PATH_BY_DEEPLINK = '/traces/group-photos-by-deeplink.txt';
+    private const TRACE_PATH_BY_USER = '/traces/group-photos-by-user.txt';
     private const CHANNEL_TRACE_PATH = '/traces/channel-photos.txt';
     private const CHANNEL_ID = 1229718840;
     private const DEFAULT_FILE_SIZE = 140179;
@@ -34,7 +35,7 @@ class GroupPhotosScenarioTest extends TestCase
      */
     public function test_basic_get_scenario(): void {
         $count = 0;
-        $saveHandler = function (PictureModel $model, int $id) use (&$count) {
+        $saveHandler = function (PictureModel $model, /** @noinspection PhpUnusedParameterInspection */ int $id) use (&$count) {
             $count++;
             $this->assertEquals(self::DEFAULT_FILE_SIZE, strlen($model->bytes));
             $this->assertEquals(1578494852, $model->modificationTime);
@@ -43,7 +44,7 @@ class GroupPhotosScenarioTest extends TestCase
         $basicGenerator = new NullBasicClientGenerator(json_decode($file, true));
         $authKey = self::DEFAULT_AUTHKEY;
         $testGenerator = new TestClientGenerator($basicGenerator, $authKey);
-        $client = new GroupPhotosScenario(new OptionalDateRange(), $saveHandler, $testGenerator);
+        $client = new GroupPhotosScenario(new OptionalDateRange(), null, $saveHandler, $testGenerator);
         $client->setTimeout(self::TIMEOUT);
         $client->startActions();
         $this->assertEquals(1, $count);
@@ -57,7 +58,7 @@ class GroupPhotosScenarioTest extends TestCase
      */
     public function test_channel_get_scenario(): void {
         $count = 0;
-        $saveHandler = function (PictureModel $model, int $id) use (&$count) {
+        $saveHandler = function (PictureModel $model, /** @noinspection PhpUnusedParameterInspection */ int $id) use (&$count) {
             $count++;
             $this->assertEquals(self::DEFAULT_FILE_SIZE, strlen($model->bytes));
             $this->assertEquals(1578467676, $model->modificationTime);
@@ -66,7 +67,7 @@ class GroupPhotosScenarioTest extends TestCase
         $basicGenerator = new NullBasicClientGenerator(json_decode($file, true));
         $authKey = self::DEFAULT_AUTHKEY;
         $testGenerator = new TestClientGenerator($basicGenerator, $authKey);
-        $client = new GroupPhotosScenario(new OptionalDateRange(), $saveHandler, $testGenerator);
+        $client = new GroupPhotosScenario(new OptionalDateRange(), null, $saveHandler, $testGenerator);
         $client->setGroupId(self::CHANNEL_ID);
         $client->setTimeout(self::TIMEOUT);
         $client->startActions();
@@ -81,7 +82,7 @@ class GroupPhotosScenarioTest extends TestCase
      */
     public function test_channel_get_with_time_limit_scenario(): void {
         $count = 0;
-        $saveHandler = function (PictureModel $model, int $id) use (&$count) {
+        $saveHandler = function (PictureModel $model, /** @noinspection PhpUnusedParameterInspection */ int $id) use (&$count) {
             $count++;
             $this->assertEquals(self::DEFAULT_FILE_SIZE, strlen($model->bytes));
             $this->assertEquals(1578467676, $model->modificationTime);
@@ -95,6 +96,7 @@ class GroupPhotosScenarioTest extends TestCase
                 self::START_TS_20190101, // 20190101
                 self::END_TS_20200202 // 20200202
             ),
+            null,
             $saveHandler,
             $testGenerator
         );
@@ -112,7 +114,7 @@ class GroupPhotosScenarioTest extends TestCase
      */
     public function test_channel_get_with_time_limit_scenario_empty(): void {
         $count = 0;
-        $saveHandler = function (PictureModel $model, int $id) use (&$count) {
+        $saveHandler = function (PictureModel $model, /** @noinspection PhpUnusedParameterInspection */ int $id) use (&$count) {
             $count++;
             $this->assertEquals(self::DEFAULT_FILE_SIZE, strlen($model->bytes));
             $this->assertEquals(1578467676, $model->modificationTime);
@@ -126,6 +128,7 @@ class GroupPhotosScenarioTest extends TestCase
                 self::START_TS_20190101, // 20190101
                 self::END_TS_20190202 // 20190202
             ),
+            null,
             $saveHandler,
             $testGenerator
         );
@@ -143,7 +146,10 @@ class GroupPhotosScenarioTest extends TestCase
      */
     public function test_basic_get_with_group_scenario_without_limits(): void {
         $count = 0;
-        $saveHandler = function (PictureModel $model, int $id) use (&$count) {
+        $saveHandler = function (
+/** @noinspection PhpUnusedParameterInspection */ PictureModel $model,
+/** @noinspection PhpUnusedParameterInspection */ int $id
+        ) use (&$count) {
             $count++;
         };
         $file = file_get_contents(__DIR__.self::TRACE_PATH_BY_DEEPLINK);
@@ -152,6 +158,7 @@ class GroupPhotosScenarioTest extends TestCase
         $testGenerator = new TestClientGenerator($basicGenerator, $authKey);
         $client = new GroupPhotosScenario(
             new OptionalDateRange(),
+            null,
             $saveHandler,
             $testGenerator
         );
@@ -169,7 +176,10 @@ class GroupPhotosScenarioTest extends TestCase
      */
     public function test_basic_get_with_group_scenario(): void {
         $count = 0;
-        $saveHandler = function (PictureModel $model, int $id) use (&$count) {
+        $saveHandler = function (
+/** @noinspection PhpUnusedParameterInspection */ PictureModel $model,
+/** @noinspection PhpUnusedParameterInspection */ int $id
+        ) use (&$count) {
             $count++;
         };
         $file = file_get_contents(__DIR__.self::TRACE_PATH_BY_DEEPLINK);
@@ -178,6 +188,37 @@ class GroupPhotosScenarioTest extends TestCase
         $testGenerator = new TestClientGenerator($basicGenerator, $authKey);
         $client = new GroupPhotosScenario(
             new OptionalDateRange(),
+            null,
+            $saveHandler,
+            $testGenerator
+        );
+        $client->setDeepLink(self::DEFAULT_GROUP_DEEPLINK);
+        $client->setTimeout(self::TIMEOUT);
+        $client->startActions();
+        $this->assertEquals(2, $count);
+    }
+
+    /**
+     * We expect two photos to be loaded in basic scenario with chat url and user filter
+     *
+     * @throws TGException
+     * @noinspection PhpUnusedParameterInspection
+     */
+    public function test_basic_get_with_user_scenario(): void {
+        $count = 0;
+        $saveHandler = function (
+/** @noinspection PhpUnusedParameterInspection */ PictureModel $model,
+/** @noinspection PhpUnusedParameterInspection */ int $id
+        ) use (&$count) {
+            $count++;
+        };
+        $file = file_get_contents(__DIR__.self::TRACE_PATH_BY_USER);
+        $basicGenerator = new NullBasicClientGenerator(json_decode($file, true));
+        $authKey = self::DEFAULT_AUTHKEY;
+        $testGenerator = new TestClientGenerator($basicGenerator, $authKey);
+        $client = new GroupPhotosScenario(
+            new OptionalDateRange(),
+            'ntlvikhhjofnekge',
             $saveHandler,
             $testGenerator
         );
@@ -195,7 +236,7 @@ class GroupPhotosScenarioTest extends TestCase
      */
     public function test_basic_scenario_with_date_limit_none(): void {
         $count = 0;
-        $saveHandler = function (PictureModel $model, int $id) use (&$count) {
+        $saveHandler = function (PictureModel $model, /** @noinspection PhpUnusedParameterInspection */ int $id) use (&$count) {
             $count++;
             $this->assertEquals(self::DEFAULT_FILE_SIZE, strlen($model->bytes));
             $this->assertEquals(1578494852, $model->modificationTime);
@@ -209,6 +250,7 @@ class GroupPhotosScenarioTest extends TestCase
                 self::START_TS_20190101, // 20190101
                 self::END_TS_20190202 // 20190202
             ),
+            null,
             $saveHandler,
             $testGenerator
         );
@@ -225,7 +267,10 @@ class GroupPhotosScenarioTest extends TestCase
      */
     public function test_basic_scenario_with_date_limit_one(): void {
         $count = 0;
-        $saveHandler = function (PictureModel $model, int $id) use (&$count) {
+        $saveHandler = function (
+/** @noinspection PhpUnusedParameterInspection */ PictureModel $model,
+/** @noinspection PhpUnusedParameterInspection */ int $id
+        ) use (&$count) {
             $count++;
         };
         $file = file_get_contents(__DIR__.self::DEFAULT_TRACE_PATH);
@@ -237,6 +282,7 @@ class GroupPhotosScenarioTest extends TestCase
                 self::START_TS_20190101, // 20190101
                 self::END_TS_20200202 // 20200202
             ),
+            null,
             $saveHandler,
             $testGenerator
         );
