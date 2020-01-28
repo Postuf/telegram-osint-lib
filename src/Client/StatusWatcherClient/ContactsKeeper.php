b7555e3fea9e788459d21278e2acfa0b5fa4da65
Mikhail Alferov <alferov.m@postuf.com>
2020-01-28 11:11:49 +0300

    Clear code

--- src/Client/StatusWatcherClient/ContactsKeeper.php
+++ src/Client/StatusWatcherClient/ContactsKeeper.php
@@ -242,7 +242,6 @@ class ContactsKeeper
     public function delNumbers(array $numbers, callable $onComplete)
     {
         $this->getUsersByPhones($numbers, function (array $contacts) use ($numbers, $onComplete) {
-            sleep(4);
             // if all current contacts to be deleted
             if(count($contacts) == count($this->contacts))
                 $this->cleanContacts($onComplete);
