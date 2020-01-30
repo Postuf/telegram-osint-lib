7857e514751a1e9c3c020ae15c16d68c53c3eae8
wkejqb <58689118+wkejqb@users.noreply.github.com>
2020-01-30 12:25:56 +0300

    File caching map (#79)

---
+++ src/Tools/CacheMap.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TelegramOSINT\Tools;
+
+class CacheMap
+{
+    /** @var string */
+    private $filename;
+    /** @var array */
+    private $map = [];
+
+    public function __construct(string $filename)
+    {
+        if (file_exists($filename)) {
+            $this->map = json_decode(file_get_contents($filename), true);
+        }
+    }
+
+    public function set(string $key, $value): void {
+        $this->map[$key] = $value;
+        file_put_contents($this->filename, json_encode($this->map, JSON_PRETTY_PRINT));
+    }
+
+    public function get(string $key) {
+        return $this->map[$key] ?: null;
+    }
+}
