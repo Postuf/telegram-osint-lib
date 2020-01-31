266cc02b4b7e0f9f363f05cccede25ce98f7ab4c
wkejqb <58689118+wkejqb@users.noreply.github.com>
2020-01-31 17:57:10 +0300

    Improve test coverage (#84)
    - add tools tests

---
+++ tests/Unit/Tools/UsernameTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit\Tools;
+
+use PHPUnit\Framework\TestCase;
+use TelegramOSINT\Tools\Username;
+
+class UsernameTest extends TestCase
+{
+    public function test_username_equals(): void
+    {
+        $un1 = 'xxx';
+        $un2 = 'xxx ';
+        $this->assertEquals(true, Username::equal($un1, $un2));
+    }
+
+    public function test_username_not_equals(): void
+    {
+        $un1 = 'xxx';
+        $un2 = 'xxy ';
+        $this->assertEquals(false, Username::equal($un1, $un2));
+    }
+}
