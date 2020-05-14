869e39d4bac1d57afbd4c9784f61f383c7bce8c3
Quisonann <quisonann@postuf.com>
2020-05-14 12:48:31 +0300

    UserResolveScenario updated for standalone execution
    - send_message node added

---
+++ src/TLMessage/TLMessage/ClientMessages/send_message.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TelegramOSINT\TLMessage\TLMessage\ClientMessages;
+
+use TelegramOSINT\TLMessage\TLMessage\Packer;
+use TelegramOSINT\TLMessage\TLMessage\TLClientMessage;
+
+/**
+ * @see https://core.telegram.org/method/messages.sendMessage
+ */
+class send_message implements TLClientMessage
+{
+    private const CONSTRUCTOR = 1376532592; // 0x520c3870
+
+    /** @var input_peer */
+    private $inputPeer;
+    /** @var string */
+    private $message;
+
+    public function __construct(input_peer $inputPeer, string $message)
+    {
+        $this->inputPeer = $inputPeer;
+        $this->message = $message;
+    }
+
+    public function getName(): string
+    {
+        return 'send_message';
+    }
+
+    public function toBinary(): string
+    {
+        return Packer::packConstructor(self::CONSTRUCTOR).
+            Packer::packInt(0). // flags not implemented
+            $this->inputPeer->toBinary().
+            Packer::packString($this->message).
+            Packer::packLong(rand());
+    }
+}
