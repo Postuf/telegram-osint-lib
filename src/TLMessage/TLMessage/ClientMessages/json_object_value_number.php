bccbb9eaea38eaaae3fedeecb1dec6c34da86af1
Qusonann <58689118+Qusonann@users.noreply.github.com>
2020-06-03 13:45:47 +0300

    Fixed broken registration (#145)
    - since year 2020 initConnection was extended with params containing apk signature

---
+++ src/TLMessage/TLMessage/ClientMessages/json_object_value_number.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TelegramOSINT\TLMessage\TLMessage\ClientMessages;
+
+use TelegramOSINT\TLMessage\TLMessage\Packer;
+
+/**
+ * https://core.telegram.org/constructor/jsonNumber
+ */
+class json_object_value_number extends json_object_value_abstract
+{
+    private const CONSTRUCTOR = 736157604; // 0x2be0dfa4
+
+    /** @var float */
+    private $value;
+
+    public function __construct(float $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getName(): string
+    {
+        return 'json_object_value_number';
+    }
+
+    public function toBinary(): string
+    {
+        return Packer::packConstructor(self::CONSTRUCTOR).
+            Packer::packDouble($this->value);
+    }
+}
