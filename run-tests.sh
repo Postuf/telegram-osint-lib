89998c29b668e425578624087bcebc5aa8cd511d
Qusonann <58689118+Qusonann@users.noreply.github.com>
2020-06-23 15:23:42 +0300

    Gitpod support (#159)
    - web-based ide, can be opened directly from github

--- run-tests.sh
+++ run-tests.sh
@@ -8,4 +8,4 @@ nohup node node/proxy.js &
 echo $! > pid1.txt
 
 cd tests
-../vendor/bin/phpunit --configuration phpunit.config.xml . && kill -9 `cat ../pid1.txt`
+../vendor/bin/phpunit --configuration phpunit.config.xml . && kill -9 `cat ../pid1.txt` && rm -f ../pid1.txt ../nohup.out
