d637bf659c8717e373229ffeee471cbae5125073
Anton <Antochi.A@postuf.com>
2021-12-21 14:20:44 +0300

    Update 8.3.2 version

--- src/LibConfig.php
+++ src/LibConfig.php
@@ -48,13 +48,13 @@ class LibConfig
 
     public const APP_DEFAULT_DEVICE_LANG_CODE = 'en-us';
     public const APP_DEFAULT_LANG_CODE = 'en';
-    public const APP_DEFAULT_VERSION = '7.8.0';
+    public const APP_DEFAULT_VERSION = '8.3.2';
     // see https://www.apkmirror.com/apk/telegram-fz-llc/telegram/telegram-6-0-1-release/
     // arm64-v8a for android 6+ has 5th digit always "7"
-    public const APP_DEFAULT_VERSION_CODE = '23607';
+    public const APP_DEFAULT_VERSION_CODE = '24967';
     public const APP_DEFAULT_LANG_PACK = 'android';
     // https://schema.horner.tj
-    public const APP_DEFAULT_TL_LAYER_VERSION = 130;
+    public const APP_DEFAULT_TL_LAYER_VERSION = 135;
 
     public const ENV_AUTHKEY = 'BOT'; // env variable for authkey path
 }
