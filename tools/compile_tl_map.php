d3c86291bfd3c34ef8d657f2eb7ede5694a5d588
Александр Клешнин <kleshnin.a@postuf.com>
2022-06-10 19:18:54 +0300

    Changed layer compile process
    - Added layer_compiled_140.json as a base layer
    - Layers with a large number will be added to compiled one on top in ascending order
    - deleted old layer files

--- tools/compile_tl_map.php
+++ tools/compile_tl_map.php
@@ -16,17 +16,45 @@ function getMapNodes(string $filename, array &$map)
     }
 }
 
-$mapStore = __DIR__.'/../src/MTSerialization/OwnImplementation/maps';
+$mapStore = __DIR__ . '/../src/MTSerialization/OwnImplementation/maps';
 $maps = array_diff(
     scandir($mapStore),
     ['.', '..']
 );
-$resultMap = [];
+
+// find layer_compiled_XXX.json with max layer and sort simple layer files (layer_XXX.json) by version
+$compiledLayerFiles = [];
+$simpleLayerFiles = [];
 foreach ($maps as $mapFile) {
-    $map = getMapNodes($mapStore.'/'.$mapFile, $resultMap);
+    preg_match("/^layer_compiled_(\d+)\.json$/i", $mapFile, $matches);
+    if (sizeof($matches) > 0)
+        $compiledLayerFiles[$matches[1]] = $mapFile;
+    preg_match("/^layer_(\d+)\.json$/i", $mapFile, $matches);
+    if (sizeof($matches) > 0)
+        $simpleLayerFiles[$matches[1]] = $mapFile;
+}
+krsort($compiledLayerFiles);
+ksort($simpleLayerFiles);
+
+
+$resultMap = ['constructors' => [], 'methods' => []];
+$lastLayer = 0;
+// last compiled layer file is the most recent
+foreach ($compiledLayerFiles as $layer => $mapFile) {
+    getMapNodes($mapStore . '/' . $mapFile, $resultMap);
+    $lastLayer = $layer;
+    echo "Layer $layer\n";
+    break;
+}
+// process simple layer files greater than last compiled layer
+foreach ($simpleLayerFiles as $layer => $mapFile) {
+    if ($layer > $lastLayer) {
+        getMapNodes($mapStore . '/' . $mapFile, $resultMap);
+        echo "Layer $layer\n";
+    }
 }
 
 $resultMap['constructors'] = array_values($resultMap['constructors']);
 $resultMap['methods'] = array_values($resultMap['methods']);
 $compiled = json_encode($resultMap, JSON_PRETTY_PRINT);
-file_put_contents($mapStore.'/compiled.json', $compiled);
+file_put_contents($mapStore . '/compiled.json', $compiled);
