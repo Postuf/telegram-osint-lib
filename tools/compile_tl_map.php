6b864e21f79791365c59fc87354c7a116d9e20d6
Danealau <57352065+Danealau@users.noreply.github.com>
2022-06-17 18:19:56 +0300

    Update layers (#236)
    * Changed layer compile process
    
    - Added layer_compiled_140.json as a base layer
    - Layers with a large number will be added to compiled one on top in ascending order
    - deleted old layer files
    
    * Fix layer
    
    * Add support for "flagsXXX.index?type"
    
    * Added 1-14 layers
    
    * styleCI
    
    * styleCI
    
    * add comment
    
    * add exception
    
    * add exception
    
    * style

--- tools/compile_tl_map.php
+++ tools/compile_tl_map.php
@@ -1,5 +1,7 @@
 <?php
 
+const OUTPUT_FILE = 'compiled.json';
+
 function getMapNodes(string $filename, array &$map)
 {
     $raw_entities = json_decode(file_get_contents($filename), true);
@@ -17,16 +19,54 @@ function getMapNodes(string $filename, array &$map)
 }
 
 $mapStore = __DIR__.'/../src/MTSerialization/OwnImplementation/maps';
+// delete previous compiled file
+@unlink($mapStore.'/'.OUTPUT_FILE);
+
 $maps = array_diff(
     scandir($mapStore),
     ['.', '..']
 );
-$resultMap = [];
+
+// find layer_compiled_XXX.json with max layer and sort simple layer files (layer_XXX.json) by version
+// TlObject can change fields without change TL id
+$compiledLayerFiles = [];
+$simpleLayerFiles = [];
 foreach ($maps as $mapFile) {
-    $map = getMapNodes($mapStore.'/'.$mapFile, $resultMap);
+    preg_match("/^layer_compiled_(\d+)\.json$/i", $mapFile, $matches);
+    if (sizeof($matches) > 0) {
+        $compiledLayerFiles[$matches[1]] = $mapFile;
+    }
+    preg_match("/^layer_(\d+)\.json$/i", $mapFile, $matches);
+    if (sizeof($matches) > 0) {
+        $simpleLayerFiles[$matches[1]] = $mapFile;
+    }
+}
+
+if (sizeof($maps) !== (sizeof($compiledLayerFiles) + sizeof($simpleLayerFiles))) {
+    throw new Exception('Found files with invalid names in layer dir');
+}
+
+krsort($compiledLayerFiles);
+ksort($simpleLayerFiles);
+
+$resultMap = ['constructors' => [], 'methods' => []];
+$lastLayer = 0;
+// last compiled layer file is the most recent
+foreach ($compiledLayerFiles as $layer => $mapFile) {
+    getMapNodes($mapStore.'/'.$mapFile, $resultMap);
+    $lastLayer = $layer;
+    echo "Layer $layer\n";
+    break;
+}
+// process simple layer files greater than last compiled layer
+foreach ($simpleLayerFiles as $layer => $mapFile) {
+    if ($layer > $lastLayer) {
+        getMapNodes($mapStore.'/'.$mapFile, $resultMap);
+        echo "Layer $layer\n";
+    }
 }
 
 $resultMap['constructors'] = array_values($resultMap['constructors']);
 $resultMap['methods'] = array_values($resultMap['methods']);
 $compiled = json_encode($resultMap, JSON_PRETTY_PRINT);
-file_put_contents($mapStore.'/compiled.json', $compiled);
+file_put_contents($mapStore.'/'.OUTPUT_FILE, $compiled);
